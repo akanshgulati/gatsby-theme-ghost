af8593c5ff8e10f6ecaa6cc70717604f882626a3
Akansh Gulati <akansh@travenues.com>
2019-12-06 01:38:16 +0530

    chore(config): Update the siteconfig, package.json and yarn lock

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Personal Blog`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Sharing the personal experience related to optimization, how-to, etc.`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Gatsby Theme Ghost`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Minimalistic Gatsby theme for blog`, // This allows an alternative site description for meta data for pages.
     siteTwitterHandle: `@akanshgulati`,
     siteFacebookHandle: `akanshgulati`,
     language: `en`,
@@ -12,13 +12,13 @@ module.exports = {
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Akansh-Personal Blog`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Personal Blog`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
     cover_image: ``,
 
-    logo: `/images/logo.png`,
+    logo: `/images/logo.svg`,
 
     navigation: [
         {
@@ -33,5 +33,5 @@ module.exports = {
 
     googleAnalyticsId: "", // ADD_COMPLETE_TRACKING_ID => UA-151385213-1
 
-    disqusShortname: "" // ADD_SHORT_NAME => https-try-akansh-com`
+    disqusShortname: "" // ADD_SHORT_NAME => https://help.disqus.com/en/articles/1717111-what-s-a-shortname
 }
